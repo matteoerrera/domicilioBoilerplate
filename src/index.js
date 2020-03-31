391e84a03a5bd89f25cc4e9b7dca1bbf0c68e8e9
Luca Cardelli <cardelliluca94@gmail.com>
2020-03-31 19:41:29 +0200

    removed pre-render to make hashHistory work (#2)
    Co-authored-by: Luca Cardelli <lucacardelli@MacBook-Pro-di-Luca.local>

--- src/index.js
+++ src/index.js
@@ -66,17 +66,12 @@ export default class App extends Component {
 			});
 	}
 
-	createHistory() {
-      if (typeof window !== "undefined"){
-         createHashHistory();
-      }
-	}
 
 	render(props, { isHomepage, results }) {
 		return (
 			<div id="app" class="px-5 max-w-screen-md mx-auto">
             <div>
-				<Router history={this.createHistory()} onChange={this.handleRoute}>
+				<Router history={createHashHistory()} onChange={this.handleRoute}>
 					<Home path="/" results={results} />
 					<Form path="/iniziativa" />
 				</Router>
