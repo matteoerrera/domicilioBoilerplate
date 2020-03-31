53cf62c3507b59602518020e622bbf1fb3ef11c6
Luca Cardelli <cardelliluca94@gmail.com>
2020-03-31 19:26:34 +0200

    added router hash (#1)
    * added router hash
    
    * fixed build error
    
    Co-authored-by: Luca Cardelli <lucacardelli@MacBook-Pro-di-Luca.local>

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import {h, Component, Fragment} from 'preact';
 import { Router } from 'preact-router';
+import { createHashHistory } from 'history';
 import { Link } from 'preact-router/match';
 
 import 'tailwindcss/dist/tailwind.min.css';
@@ -65,11 +66,17 @@ export default class App extends Component {
 			});
 	}
 
+	createHistory() {
+      if (typeof window !== "undefined"){
+         createHashHistory();
+      }
+	}
+
 	render(props, { isHomepage, results }) {
 		return (
 			<div id="app" class="px-5 max-w-screen-md mx-auto">
             <div>
-				<Router onChange={this.handleRoute}>
+				<Router history={this.createHistory()} onChange={this.handleRoute}>
 					<Home path="/" results={results} />
 					<Form path="/iniziativa" />
 				</Router>
